bb5f0fd60fe69e124be2fcf0f949898c985547e9
houssem <houssem.regaieg@etudiant-enit.utm.tn>
2023-09-26 21:42:39 +0100

    home and about are finished

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Logo from "./../../images/houssem.jpg"
+import {Link} from 'react-router-dom'
+import "./about.css"
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <Container className='home-container mt-2'>
+      <Row>
+        <Col>
+          <h1 className='first-title'>About <span className='me'>Me</span></h1>
+          <div><img src={Logo} alt="logo" className='image' /></div>
+          <h3 className='second-title'>Software Engineer</h3>
+          <p className="about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minus dicta assumenda et vero quis repudiandae, voluptatum deleniti eveniet perferendis numquam delectus est repellendus nam? Quasi porro inventore perferendis ducimus!
+          Magnam distinctio enim unde eius explicabo molestias doloribus nulla officiis neque amet porro, pariatur ad expedita officia possimus in! Magni odit explicabo itaque error quos facilis dolores harum temporibus? Exercitationem.
+          Repellendus ratione debitis quisquam quis recusandae ad ullam sed voluptatibus inventore rem dolore adipisci voluptates praesentium sunt blanditiis ducimus in id obcaecati quia, pariatur reprehenderit mollitia culpa. Nihil, possimus praesentium!</p>
+          <Link to="/education" className='btn btn-outline-primary btn-lg link-button'>Read More</Link>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,7 +7,9 @@ import Logo from "./../../images/houssem.jpg"
 import Typed from 'react-typed';
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom'
-
+import Github from "./../../images/github.png"
+import Linkedin from "./../../images/linkdeen.png"
+import Facebook from "./../../images/facebook.png"
 export default function Home() {
   return (
     <Container className='home-container'>
@@ -28,12 +30,16 @@ export default function Home() {
           Molestias, error, est molestias obcaecati dolores cumque,
           Commodi rente voluatibus?</p>
           <div className="buttons text-center me-auto">
-            <Button variant="outline-primary" className='cv-button' size='lg'>Download CV</Button>
-            <Button variant="outline-primary" className='contact' size='lg'><Link to="/contact" className='contact-link'>Contact Me</Link></Button>
+            <Button variant="outline-primary" className='cv-button btn-lg'>Download CV</Button>
+            <Link to="/contact" className='btn btn-outline-primary btn-lg link-button'>Contact Me</Link>
           </div>
-          <div className="social-media"></div>
+          <div className="social-media">
+            <a href="https://www.linkedin.com/in/houssem-regaieg-589526258/" target="_blank" rel="noreferrer"><img src={Linkedin} alt="linkedin-icon" className='linkedin-icon' /></a>
+            <a href="https://www.facebook.com/profile.php?id=100009183349705" target="_blank" rel="noreferrer"><img src={Facebook} alt="facebook-icon" className='facebook-icon' /></a>
+            <a href="https://github.com/HoussemRg" target="_blank" rel="noreferrer"><img src={Github} alt="github-icon" className='github-icon'/></a>
+          </div>
         </Col>
-        <Col sm={4} className="text-center"><img src={Logo} alt="logo" className='image' /></Col>
+        <Col sm={4} className="text-center align-self-center"><img src={Logo} alt="logo" className='image' /></Col>
       </Row>
     </Container>
   )
